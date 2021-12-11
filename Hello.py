bbbb506b0ac99ed9f06786736dc0afeec220cbf8
takuya <tkykwsm6ch@gmail.com>
2021-12-11 21:59:44 +0900

    update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
-print("Hello")
+import plotly.graph_objects as go
+
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(x=[1, 2, 3], y=[40, 25, 60]),
+)
+fig.show()
+
+fig.write_html("test.html") # test.htmlとして保存
